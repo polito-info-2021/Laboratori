eb555d98d6067536790731e8519e57d41bdaa973
Fulvio Corno <fulvio.corno@polito.it>
2021-11-05 12:00:26 +0100

    Vecchia versione soluzioni, riformattate

---
+++ lab07 (soluzioni)/es8-smooth.py
@@ -0,0 +1,42 @@
+"""
+Smooth out values in a list by averaging them with their neighbors.
+"""
+
+
+def main():
+    values = [1, 2, 3, 5, 3, 1, 4]
+
+    # Display the original values.
+    print("The original values are:", values)
+
+    # Smooth the values and display the result.
+    smooth(values)
+    print("The smoothed values are:", values)
+
+
+def smooth(data):
+    """
+    Smooth values in a list by averaging the value of each element with its neighbor(s).
+
+    :param data: data the list of values to smooth
+    :return: None (it modifies the list in-place)
+    """
+    if len(data) <= 1:
+        return
+
+    # Handle the first element in the list.
+    old_left = data[0]
+    data[0] = (data[0] + data[1]) / 2
+
+    # Handle all values except for the first and last.
+    for i in range(1, len(data) - 1):
+        current = data[i]
+        data[i] = (old_left + data[i] + data[i + 1]) / 3
+        old_left = current
+
+    # Handle the last element in the list.
+    data[len(data) - 1] = (old_left + data[len(data) - 1]) / 2
+
+
+# Call the main function.
+main()
